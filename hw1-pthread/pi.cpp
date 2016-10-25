909e640203d1b3cb38c8aa0fae8fd712f421f3cf
Kiwi <kiwi010379@gmail.com>
2016-10-25 13:07:08 +0800

    Add argc

---
+++ hw1-pthread/pi.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+
+long long in_circle = 0;
+long long total = 0;
+
+int main(int argc, char *argv[]){
+  
+  total = atoi( argv[1] );
+  printf("total num of tosses = %lli\n", total);
+  
+}
