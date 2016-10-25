9dce24b30db235b98251427aadef379084bd66ab
Kiwi <kiwi010379@gmail.com>
2016-10-25 17:02:24 +0800

    Add makefile

--- hw1-pthread/pi.cpp
+++ hw1-pthread/pi.cpp
@@ -8,8 +8,9 @@ long long in_circle = 0;
 long long total = 0;
 
 int main(int argc, char *argv[]){
-  
   total = atoi( argv[1] );
   printf("total num of tosses = %lli\n", total);
   
+//To be scalable, need to know the number of processors
+
 }
