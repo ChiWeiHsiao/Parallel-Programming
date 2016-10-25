b2909187ef30a220bb6b0d7dd07fd60f7f31b680
Kiwi <kiwi010379@gmail.com>
2016-10-25 18:07:37 +0800

    Add pi calculation without parallel
    The estimated pi is about 3.9
    It seems that x and y is not quite random.

--- hw1-pthread/pi.cpp
+++ hw1-pthread/pi.cpp
@@ -10,7 +10,17 @@ long long total = 0;
 int main(int argc, char *argv[]){
   total = atoi( argv[1] );
   printf("total num of tosses = %lli\n", total);
-  
+
+  for(int i = 0; i < total; i++){
+    //create random (x.y) between 1 ~ -1
+    double x = (double)(rand() % RAND_MAX) / RAND_MAX - (double)(rand() % RAND_MAX) / RAND_MAX;
+    double y = (double)(rand() % RAND_MAX) / RAND_MAX - (double)(rand() % RAND_MAX) / RAND_MAX;
+    cout << i <<": "<<"x=" <<x <<"; y=" << y<< endl;
+    double r = x*x + y*y;
+    if(r <= 1)  in_circle++;
+  }  
+  double pi = 4 * in_circle / ((double)total);
+  cout << "Estimated pi = " << pi <<endl;
 //To be scalable, need to know the number of processors
 
 }
